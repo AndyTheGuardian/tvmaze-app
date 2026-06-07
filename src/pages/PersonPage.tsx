d451e78aaca9ea3f914b6b7ab3f958df27c7dc9b
AndyTheGuardian <andytheguardian@hotmail.de>
2026-06-08 00:55:12 +0200

    Added optional inside breaks

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -4,6 +4,7 @@ import { usePerson } from "../hooks/usePerson";
 import { ShowCard } from "../components/ShowCard";
 import { VenetianMask } from "lucide-react";
 import { groupedCastCreditsByShow } from "../utils/groupedCastCreditsByShow";
+import { useState } from "react";
 
 export function PersonPage() {
   const { id } = useParams();
@@ -13,6 +14,8 @@ export function PersonPage() {
   const { data: person } = usePerson(personId);
 
   const { data: credits = [] } = useCastCredits(personId);
+
+  const [inlineBreaks, setInlineBreaks] = useState(true);
 
   const uniqueShows = Array.from(
     new Map(
@@ -145,17 +148,21 @@ export function PersonPage() {
               md:rounded-bl-none
               md:rounded-r-lg"
             >
-              <h2 className="mb-1 font-bold">Credits</h2>
+              <h2
+                className="mb-1 font-bold"
+                onClick={() => setInlineBreaks(!inlineBreaks)}
+              >
+                Credits
+              </h2>
               {/* // grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-x-2 gap-y-1
                 // [column-width:150px] gap-4     */}
               <div className="columns-2 sm:columns-3 md:columns-4 gap-4 [column-fill:balance]">
                 {groupedCredits?.map(({ show, characters, self, voice }) => (
-                  <Link
-                    key={show.id}
-                    to={`/show/${show.id}`}
-                    className="mb-2 block break-inside-avoid"
-                  >
-                    <div id={show.id.toString()} className="gap-2 text-sm">
+                  <Link key={show.id} to={`/show/${show.id}`} className="">
+                    <div
+                      key={show.id}
+                      className={`text-sm mb-2 block ${inlineBreaks ? "" : "break-inside-avoid"}`}
+                    >
                       <div className="opacity-70 font-semibold">
                         {show.name}
                         {self && (
